6bbd833958fa5b39489acb5a2f50cad856ac004d
Vinu Rakav S <63595155+LordofUniverse@users.noreply.github.com>
2022-11-23 14:46:14 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+import base64
+from PIL import Image
+from io import BytesIO
+
+with open("test.jpeg", "rb") as image_file:
+    data = base64.b64encode(image_file.read())
+
+    print(data)
+
+im = Image.open(BytesIO(base64.b64decode(data)))
+im.save('image1.png', 'PNG')
